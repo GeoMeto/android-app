13e7e9a05eb32424e2f955a92ded604cd797a696
Georgi Metodiev <georgi.metodiev@effortel.com>
2022-05-26 14:47:58 +0300

    Adds auth logic

--- app/src/main/java/com/tu/challengeyourself/CreateAccountActivity.java
+++ app/src/main/java/com/tu/challengeyourself/CreateAccountActivity.java
@@ -1,26 +1,45 @@
 package com.tu.challengeyourself;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 import com.tu.challengeyourself.utils.MethodHelper;
 
 public class CreateAccountActivity extends AppCompatActivity {
-
+    private final String TAG = this.getClass().getCanonicalName();
     boolean isTermsAndConditionsChecked = false;
+    private FirebaseAuth mAuth;
+    private EditText passwordEdit, emailEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEdit = findViewById(R.id.editCreateEmail);
+        passwordEdit = findViewById(R.id.editCreatePassword);
+
+
 
         setTermsListener();
         setCreateAccListener();
@@ -31,28 +50,58 @@ public class CreateAccountActivity extends AppCompatActivity {
         createAccBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (MethodHelper.isEmailAlreadyUsed()) {
-                    showToast("This email is already used!");
-                    return;
-                }
-                if (MethodHelper.isEmailCorrupt()) {
-                    showToast("Invalid email! Please use the pattern john@example.com.");
-                    return;
-                }
-                if (MethodHelper.isPasswordWeak()) {
-                    showToast("Password is weak! Please use a stronger password.");
-                    return;
-                }
                 if (!isTermsAndConditionsChecked) {
                     showToast("You have to check the terms and conditions!");
                     return;
                 }
-                loginWithNewAccount();
+                createAccount();
             }
         });
     }
 
-    private void loginWithNewAccount() {
+    private void createAccount() {
+        String email = emailEdit.getText().toString();
+        String password = passwordEdit.getText().toString();
+        try {
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                loginWithNewAccount(user);
+                            } else {
+                                try {
+                                    throw task.getException();
+                                } catch (FirebaseAuthWeakPasswordException e) {
+                                    passwordEdit.setError("This password is weak. Please use a password with at least 6 characters.");
+                                    passwordEdit.requestFocus();
+                                    showToast("This password is weak. Please use a password with at least 6 characters.");
+                                } catch (FirebaseAuthInvalidCredentialsException e) {
+                                    emailEdit.setError("Email is not correctly formatted.");
+                                    emailEdit.requestFocus();
+                                    showToast("Email is not correctly formatted.");
+                                } catch (FirebaseAuthUserCollisionException e) {
+                                    emailEdit.setError("This user already exists. Please use the login from.");
+                                    emailEdit.requestFocus();
+                                    showToast("This user already exists. Please use the login from.");
+                                } catch (Exception e) {
+                                    showToast("Authentication failed." + e.getMessage());
+                                }
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            }
+                        }
+                    });
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(CreateAccountActivity.this, "Email or password is empty!",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void loginWithNewAccount(FirebaseUser user) {
         Intent loginIntent = new Intent(CreateAccountActivity.this, LoginActivity.class);
         loginIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(loginIntent);
--- app/src/main/java/com/tu/challengeyourself/LoginActivity.java
+++ app/src/main/java/com/tu/challengeyourself/LoginActivity.java
@@ -2,19 +2,34 @@ package com.tu.challengeyourself;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private final String TAG = this.getClass().getCanonicalName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
         setLoginListener();
         setCreateAccListener();
@@ -22,19 +37,61 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void setLoginListener() {
+        EditText emailEdit = findViewById(R.id.emailTxt);
+        EditText passEdit = findViewById(R.id.passTxt);
+        String email = emailEdit.getText().toString();
+        String password = passEdit.getText().toString();
+
         Button loginBtn = findViewById(R.id.btnLogin);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (successfulLogin()) {
+                if (isLoginSuccessful(email, password)) {
                     Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
                     mainIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(mainIntent);
-                } else {
-                    showToastWrongCredentials();
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    user.getIdToken(false);
                 }
             }
         });
+    }
+
+    private boolean isLoginSuccessful(String email, String password) {
+        final boolean[] isLoginSuccessful = {false};
+        try {
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            isLoginSuccessful[0] = task.isSuccessful();
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                            } else {
+                                try {
+                                    throw task.getException();
+                                } catch (FirebaseAuthInvalidUserException e) {
+                                    showToast("User not found. Please create a new account.");
+                                } catch (FirebaseAuthInvalidCredentialsException e) {
+                                    showToast("Invalid password. Please try again or reset password.");
+                                } catch (Exception e) {
+                                    showToast("Authentication failed. Error message:" + task.getException().getMessage());
+                                }
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            }
+                        }
+                    });
+        } catch (IllegalArgumentException e) {
+            Toast.makeText(getApplicationContext(), "Email or password is empty!",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return isLoginSuccessful[0];
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
     }
 
     private void setCreateAccListener() {
@@ -57,13 +114,5 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(createAccIntent);
             }
         });
-    }
-
-    private void showToastWrongCredentials() {
-        Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
-    }
-
-    private boolean successfulLogin() {
-        return true;
     }
 }
